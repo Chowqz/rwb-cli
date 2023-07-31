aabfa11134a56e34a469bc47b13bbf64fe3f7675
qiongzhong.zhou <qiongzhong.zhou@shopee.com>
2023-07-31 15:09:21 +0800

    feat: feature-02-commit-02

--- bin/index.js
+++ bin/index.js
@@ -97,3 +97,5 @@ program
   });
 
 program.parse(process.argv);
+
+console.log("feature-02-commit-02");
--- bin/test02.js
+++ bin/test02.js
@@ -1,2 +1,5 @@
 const a = "feature-02-commit-01";
 console.log(a);
+
+const b = "febture-02-commit-02";
+console.log(b);
